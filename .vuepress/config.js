9bdcb2e2f8adcd7e8e90f64a9a7d775dc531700f
jerryzhang <jerryzhang@muchenglin.com>
2019-11-08 19:12:30 +0800

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 站点配置
+    plugins: [
+        [
+            'vuepress-plugin-yuque',
+            {
+                repoUrl: 'https://www.yuque.com/endday/blog',
+                source: 'markdown'
+            }
+        ]
+    ]
+}
